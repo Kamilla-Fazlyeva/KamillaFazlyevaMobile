d7ec5fd8f286133907669d103035c912070c735e
Kamilla-Fazlyeva <flora666@yandex.ru>
2020-05-26 12:16:34 +0400

    initial

---
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -0,0 +1,13 @@
+package scenarios;
+
+import org.testng.annotations.Test;
+import setup.BaseTest;
+
+public class nativeMobileTests extends BaseTest {
+
+    @Test(groups = {"native"}, description = "This simple test just click on the Sign In button")
+    public void simpleNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+        getPageObject().getWelement("signInBtn").click();
+        System.out.println("Simplest Android native test done");
+    }
+}
