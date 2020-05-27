0ab914da7d6a6a0be6e0fb3f146587eb0735c54f
Kamilla-Fazlyeva <flora666@yandex.ru>
2020-05-27 18:52:58 +0400

    homework_2

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -11,6 +11,35 @@ public class NativePageObject {
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
 
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registerBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement emailField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement usernameField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement passwordField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement confirmPasswordField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registerAccountBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginEmail;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement loginPassword;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout" +
+            "/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[2]" +
+            "/android.view.View/android.widget.TextView")
+    WebElement pageTitle;
+
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(new AppiumFieldDecorator(appiumDriver), this);
     }
